4f9181b03d8ab9c0d19e504ea23d91da0d1ca517
Nishanth BS <nishanth.star1@gmail.com>
2018-07-31 19:01:55 +0530

    Modified app module files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthGuard } from './shared';
+import { StudentService } from './services/student.service';
+import { TokenInterceptorService } from './services/tokeninterceptor.service'
+
+// AoT requires an exported function for factories
+export const createTranslateLoader = (http: HttpClient) => {
+    /* for development
+    return new TranslateHttpLoader(
+        http,
+        '/start-angular/SB-Admin-BS4-Angular-6/master/dist/assets/i18n/',
+        '.json'
+    ); */
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        CommonModule,
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: createTranslateLoader,
+                deps: [HttpClient]
+            }
+        }),
+        AppRoutingModule,
+        BrowserAnimationsModule
+    ],
+    declarations: [AppComponent],
+    providers: [AuthGuard,StudentService,
+    {
+        provide: HTTP_INTERCEPTORS,
+        useClass : TokenInterceptorService,
+        multi : true
+    }],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
